7233aeb93ac9cb8f783814d4b1fb03c1c642cd57
Florian Mihola <furomiho@gmail.com>
2014-11-08 17:55:56 +0100

    Create nodes for users (no duplicates)

--- aic-bigdata-server/src/main/java/aic/bigdata/extraction/handler/TweetToNeo4JHandler.java
+++ aic-bigdata-server/src/main/java/aic/bigdata/extraction/handler/TweetToNeo4JHandler.java
@@ -13,15 +13,17 @@ import java.util.HashMap;
 //import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.graphdb.index.IndexManager;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.Transaction;
 
 public class TweetToNeo4JHandler implements TweetHandler {
 	private int tweetsLogged = 0;
 	private GraphDatabaseService graphDb;
-	private ExecutionEngine cypherEngine;
-
-	// cypher queries
-	private final static String createUserQ = "CREATE ({props})";
+	private Index<Node> userIndex;
 
 /*
 	private static enum RelTypes implements RelationshipType {
@@ -29,6 +31,7 @@ public class TweetToNeo4JHandler implements TweetHandler {
 		RETWEETS
 	}
 */
+
 	public TweetToNeo4JHandler(ServerConfig config) {
 		try {
 			createDb(config.getNeo4JDbName());
@@ -36,26 +39,47 @@ public class TweetToNeo4JHandler implements TweetHandler {
 		catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
-		cypherEngine = new ExecutionEngine(graphDb);
+
+		IndexManager indexManager = graphDb.index();
+
+		try (Transaction tx = graphDb.beginTx()) {
+			userIndex = indexManager.forNodes("users");
+
+			tx.success();
+		}
 	}
 
 	@Override
 	public void HandleStatusTweet(Status status, String tweet) {
-		//System.out.println("TweetToNeo4JHandler: tweets so far: " + getCount());
-
-		// create user
-		Map<String, Object> params = new HashMap<String, Object>();
-		Map<String, Object> props = new HashMap<String, Object>();
-
 		User user = status.getUser();
-		props.put("userId", user.getId());
-		props.put("userName", user.getName());
-		params.put("props", props);
 
-		System.out.println("TweetToNeo4JHandler: Adding user \"" + user.getName() + "\" (" + user.getId() + ")");
-		cypherEngine.execute(createUserQ, params);
+		addUser(user);
 
 		tweetsLogged++;
+	}
+
+
+	private void addUser(User user) {
+		try (Transaction tx = graphDb.beginTx()) {
+			IndexHits<Node> hits = userIndex.get("userId", user.getId());
+
+			if (hits.size() == 0) {
+				Node userNode = graphDb.createNode();
+				userNode.setProperty("userId", user.getId());
+				userNode.setProperty("userName", user.getName());
+				userNode.addLabel(DynamicLabel.label("user"));
+
+				//System.out.println("TweetToNeo4JHandler: Adding user \"" + user.getName() + "\" (" + user.getId() + ")");
+
+				userIndex.add(userNode, "userId", userNode.getProperty("userId"));
+			}
+			else if (hits.size() > 1) {
+				System.out.println("TweetToNeo4JHandler: more than one user with userId " + user.getId() + " in Neo4J DB");
+			}
+			// if hits.size() was 1 we already had a node for that user
+
+			tx.success();
+		}
 	}
 
 	@Override
