bec2f96b3a90e82aa96a4505f57fa2bde6b52a06
syrenio <syrenio@gmail.com>
2014-11-30 18:13:20 +0100

    add joda-time + mongodbprovider timeoutput

--- aic-bigdata-server/src/main/java/aic/bigdata/extraction/provider/MongoDbTweetProvider.java
+++ aic-bigdata-server/src/main/java/aic/bigdata/extraction/provider/MongoDbTweetProvider.java
@@ -4,6 +4,10 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.Minutes;
+
 import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterObjectFactory;
@@ -14,41 +18,55 @@ import aic.bigdata.extraction.TweetProvider;
 import com.mongodb.DBObject;
 import com.mongodb.DBCursor;
 
-public class MongoDbTweetProvider implements TweetProvider{
+public class MongoDbTweetProvider implements TweetProvider {
 
 	private MongoDatabase db;
 	private List<TweetHandler> handler = new ArrayList<TweetHandler>();
 	private boolean running;
-	
+
 	public MongoDbTweetProvider(MongoDatabase db) {
-		this.db=db;
+		this.db = db;
 	}
-	
+
 	@Override
 	public void run() {
+		DateTime begin = new DateTime();
 		this.running = true;
+		long counter = 0;
+		long stepCounter = 0;
+		long stepSize = 10000;
+
 		try {
-			for(DBObject c : db.getCursorForTweets())
-			{
-				if(!running)
+			for (DBObject c : db.getCursorForTweets()) {
+				if (!running)
 					break;
-				
+
+				if (stepCounter >= stepSize) {
+					DateTime end = new DateTime();
+					stepCounter = 0;
+					counter++;
+					Duration diff = new Duration(begin, end);
+					System.out.println("Current Count: " + (counter * stepSize)
+							+ " Minutes:" + diff.getStandardMinutes());
+				}
+
 				String message = c.toString();
 				Status status = null;
 				try {
-					 status = TwitterObjectFactory.createStatus(message);
+					status = TwitterObjectFactory.createStatus(message);
 				} catch (TwitterException e) {
 					continue;
 				}
 				for (TweetHandler t : this.handler) {
 					t.HandleStatusTweet(status, message);
 				}
+				stepCounter++;
 			}
-		} catch (UnknownHostException e) {			
+		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void stopProvider() {
 		this.running = false;
