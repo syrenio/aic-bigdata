85439070a11c7362d158fe976efde3fef1b0bce2
syrenio <syrenio@gmail.com>
2015-01-19 20:09:32 +0100

    UserConverter now works with TweetCollection

--- aic-bigdata-server/src/main/java/aic/bigdata/stats/UserConverter.java
+++ aic-bigdata-server/src/main/java/aic/bigdata/stats/UserConverter.java
@@ -3,6 +3,9 @@ package aic.bigdata.stats;
 import java.net.UnknownHostException;
 import java.sql.SQLException;
 
+import org.joda.time.DateTime;
+import org.joda.time.Minutes;
+
 import twitter4j.TwitterException;
 import twitter4j.User;
 import aic.bigdata.database.MongoDatabase;
@@ -22,45 +25,92 @@ public class UserConverter {
 		config = new ServerConfigBuilder().getConfig();
 	}
 
+	private static void convertUsersFromUserCollection(SqlDatabase db, MongoDatabase mongo) throws SQLException {
+
+		try {
+			DBCursor cur = mongo.getCursorForUsers();
+			int max = cur.size();
+			int step = max / 10;
+			int count = 0;
+
+			System.out.println("Current user count: " + db.getUserCount());
+			System.out.print("Progress: ");
+			MongoDatabaseHelper help = new MongoDatabaseHelper();
+			while (cur.hasNext()) {
+				DBObject o = cur.next();
+				User usr = null;
+				// if (o.containsField("followersCount")) {
+				// usr = g.fromJson(o.toString(), User.class);
+				// } else {
+				// usr = TwitterObjectFactory.createUser(o.toString());
+				// }
+				usr = help.convertToUser(o);
+				AicUser x = new AicUser(usr);
+				db.createUser(x);
+				count++;
+				if (count % step == 0)
+					System.out.print("#");
+			}
+			System.out.println();
+			System.out.println("Current user count: " + db.getUserCount());
+
+		} catch (UnknownHostException | TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void convertUsersFromTweetCollection(SqlDatabase db, MongoDatabase mongo) throws SQLException {
+
+		try {
+			DBCursor cur = mongo.getCursorForTweets();
+			int max = cur.size();
+			int step = max / 10;
+			int count = 0;
+
+			System.out.println("Current user count: " + db.getUserCount());
+			System.out.print("Progress: ");
+			MongoDatabaseHelper help = new MongoDatabaseHelper();
+			while (cur.hasNext()) {
+				DBObject o = cur.next();
+				User usr = null;
+				if (o.containsField("user")) {
+					DBObject dbo = (DBObject) o.get("user");
+					usr = help.convertToUser(dbo);
+					AicUser x = new AicUser(usr);
+					db.createUser(x);
+				}
+
+				count++;
+				if (count % step == 0)
+					System.out.print("#");
+			}
+			System.out.println();
+			System.out.println("Current user count: " + db.getUserCount());
+
+		} catch (UnknownHostException | TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		SqlDatabase db;
 		try {
 			db = new SqlDatabase(config);
-			MongoDatabase mongoDatabase = new MongoDatabase(config);
-			try {
+			MongoDatabase mongo = new MongoDatabase(config);
 
-				DBCursor cur = mongoDatabase.getCursorForUsers();
-				int max = cur.size();
-				int step = max / 10;
-				int count = 0;
+			org.joda.time.DateTime start = new DateTime();
 
-				System.out.println("Current user count: " + db.getUserCount());
-				System.out.print("Progress: ");
-				MongoDatabaseHelper help = new MongoDatabaseHelper();
-				while (cur.hasNext()) {
-					DBObject o = cur.next();
-					User usr = null;
-					// if (o.containsField("followersCount")) {
-					// usr = g.fromJson(o.toString(), User.class);
-					// } else {
-					// usr = TwitterObjectFactory.createUser(o.toString());
-					// }
-					usr = help.convertToUser(o);
-					AicUser x = new AicUser(usr);
-					db.createUser(x);
-					count++;
-					if (count % step == 0)
-						System.out.print("#");
-				}
-				System.out.println();
-				System.out.println("Current user count: " + db.getUserCount());
+			// convertUsersFromUserCollection(db, mongo);
 
-			} catch (UnknownHostException | TwitterException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			convertUsersFromTweetCollection(db, mongo);
 
-			// db.getAllUsers();
+			org.joda.time.DateTime end = new DateTime();
+
+			Minutes minutesBetween = Minutes.minutesBetween(start, end);
+
+			System.out.println("Minutes: " + minutesBetween.getMinutes());
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
