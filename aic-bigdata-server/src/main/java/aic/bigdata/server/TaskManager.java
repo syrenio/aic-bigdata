21788ffb11ef549a50450c16ff185151524efc8c
syrenio <syrenio@gmail.com>
2015-01-19 20:08:17 +0100

    add Filler for Topics and Ads in TaskManager(web)

--- aic-bigdata-server/src/main/java/aic/bigdata/server/TaskManager.java
+++ aic-bigdata-server/src/main/java/aic/bigdata/server/TaskManager.java
@@ -1,12 +1,19 @@
 package aic.bigdata.server;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.sql.SQLException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
 import aic.bigdata.database.GraphDatabase;
 import aic.bigdata.database.MongoDatabase;
 import aic.bigdata.database.SqlDatabase;
+import aic.bigdata.enrichment.AdsTopicsToDatabaseFiller;
 import aic.bigdata.enrichment.TopicAnalyzer;
 import aic.bigdata.extraction.TweetHandler;
 import aic.bigdata.extraction.handler.TweetToMongoDBHandler;
@@ -78,11 +85,30 @@ public class TaskManager {
 	}
 
 	public void startAnalyse(ServerConfig cf) {
+
+		MongoDatabase b = new MongoDatabase(cf);
+		try {
+			b.removeAdsTopics();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		AdsTopicsToDatabaseFiller filler = new AdsTopicsToDatabaseFiller(cf, GraphDatabase.getInstance());
+		try {
+			filler.fillDatabase();
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+
 		analyseJob = new TopicAnalyzer(cf, GraphDatabase.getInstance());
 
 		if (executor.isShutdown())
 			executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
 		executor.execute(analyseJob);
+	}
+
+	public boolean isActive() {
+		return !executor.isShutdown() && !executor.isTerminated();
 	}
 
 }
